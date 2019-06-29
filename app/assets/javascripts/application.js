a7273484e14c1dc8b83f68aa0a9e9e663907fa76
Padeen <paulo.deen@gmail.com>
2019-06-29 06:54:23 +0200

    Add login, profile and logout links to layout Add logged_in? functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery3
+//= require bootstrap
 //= require popper
 //= require bootstrap-sprockets
 //= require rails-ujs
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
-		user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to user
@@ -14,6 +15,5 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-
   end
 end
