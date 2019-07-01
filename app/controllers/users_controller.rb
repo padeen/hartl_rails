38e637ddda86151dd24249bfdb4627aa1eb493a5
Padeen <paulo.deen@gmail.com>
2019-07-01 22:58:12 +0200

    Add before action set user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
+
   def new
     @user = User.new
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
 	def create
@@ -19,10 +21,21 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+  end
+
+	def update
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, 
