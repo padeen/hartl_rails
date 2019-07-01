9b51ea3d4e5d8cb73bd8ebecd30751711c53b778
Padeen <paulo.deen@gmail.com>
2019-07-02 00:28:35 +0200

    Add forward users after requiring login Add friendly forwarding test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def new
     @user = User.new
@@ -45,8 +46,13 @@ class UsersController < ApplicationController
 
 	def logged_in_user
 		unless logged_in?
+      store_location
 			flash[:danger] = "Please log in."
 			redirect_to login_url
 		end
+	end
+
+	def correct_user
+		redirect_to(root_url) unless current_user?(@user)
 	end
 end
