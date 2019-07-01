006e99853e4c9d573e2887e635dcfa829f38ca23
Padeen <paulo.deen@gmail.com>
2019-07-01 23:52:01 +0200

    Add access control to settings page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   before_action :set_user, only: [:show, :edit, :update]
-
 
   def new
     @user = User.new
@@ -42,4 +42,11 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, 
                                  :password_confirmation)
   end
+
+	def logged_in_user
+		unless logged_in?
+			flash[:danger] = "Please log in."
+			redirect_to login_url
+		end
+	end
 end
