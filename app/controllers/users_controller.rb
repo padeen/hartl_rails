420a7ffdb100b39fed7f4aab7caa949eec407407
Padeen <paulo.deen@gmail.com>
2019-07-02 03:15:37 +0200

    Add delete user to index Add destroy action to user controller Add access control to destroy action, only admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+	before_action :admin_user, only: :destroy
 
   def new
     @user = User.new
@@ -38,6 +39,12 @@ class UsersController < ApplicationController
     end
   end
 
+	def destroy
+    @user.destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
 
   def set_user
@@ -59,5 +66,9 @@ class UsersController < ApplicationController
 
 	def correct_user
 		redirect_to(root_url) unless current_user?(@user)
+	end
+
+	def admin_user
+		redirect_to(root_url) unless current_user.admin?
 	end
 end
