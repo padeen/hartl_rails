a98296690c2b26a432e6bd3020cca84018083c38
Padeen <paulo.deen@gmail.com>
2019-07-03 01:49:15 +0200

    Refactor email method in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  before_save :downcase_email
+  before_save   :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -39,10 +39,9 @@ class User < ApplicationRecord
   private
 
 	def downcase_email
-		self.email = email.downcase
+		self.email.downcase!
 	end
 
-	# Creates and assigns the activation token and digest.
 	def create_activation_digest
 		self.activation_token  = User.new_token
 		self.activation_digest = User.digest(activation_token)
