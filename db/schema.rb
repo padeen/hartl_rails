7276557e852d73e7504cce986f71c3af5bb897d4
Padeen <paulo.deen@gmail.com>
2019-07-02 03:02:33 +0200

    Add default value false to users admin field

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2019_07_02_005656) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
