9447eb7c9efa4e6fa1854d6906f16db2ff2238a2
Padeen <paulo.deen@gmail.com>
2019-07-03 01:26:28 +0200

    Add before callback for account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase }
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, 
@@ -8,7 +9,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
 	# Returns the hash digest of the given string.
   def User.digest(string)
@@ -34,4 +35,16 @@ class User < ApplicationRecord
 	def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
+
+	def downcase_email
+		self.email = email.downcase
+	end
+
+	# Creates and assigns the activation token and digest.
+	def create_activation_digest
+		self.activation_token  = User.new_token
+		self.activation_digest = User.digest(activation_token)
+	end
 end
