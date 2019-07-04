b4a21a4a89943ac4faf68256205ceadc1fd0cfae
Padeen <paulo.deen@gmail.com>
2019-07-04 06:42:41 +0200

    Add microposts view Add microposts variable to user controller edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,8 @@ class UsersController < ApplicationController
   end
 
   def show
+		@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def index
