10b258ece145a8b20087680fb27462479dfb2554
Padeen <paulo.deen@gmail.com>
2019-07-07 06:52:41 +0200

    Refactor move logged_in? from users controller to application controller Add microposts controller test

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :users
+  resources :microposts, only: [:create, :destroy]
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
 
