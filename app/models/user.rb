add71d5b5e5fb3fd3e8ea76ac0120f9fabe3eb88
Padeen <paulo.deen@gmail.com>
2019-07-10 03:41:12 +0200

    Add relationship to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   before_create :create_activation_digest
 
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                                foreign_key: "follower_id",
+                                                dependent:   :destroy
+
   has_secure_password
 
   attr_accessor :remember_token, :activation_token, :reset_token
