c082befbc2a4fd17c6adc1933a9370aae44183fa
Padeen <paulo.deen@gmail.com>
2019-07-10 05:50:44 +0200

    Add relationships resource for follow and unfollow actions

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       get :following, :followers
     end
   end
+  resources :relationships,       only: [:create, :destroy]
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
